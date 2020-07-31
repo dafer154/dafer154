62b8b3a584c23dfb2eb69f58f86c672ec3dcb821
andresf01 <andrewfmartinez@gmail.com>
2020-07-30 21:57:20 -0500

    Fix path to files

---
+++ precache-manifest.8e68ea6997682077968ab375589d85f9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "837ccdec57f118ebed115e7e209904ee",
+    "url": "/dafer154/index.html"
+  },
+  {
+    "revision": "908de87b908771c55d6b",
+    "url": "/dafer154/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "18ef75ec6531ad45c886",
+    "url": "/dafer154/static/css/main.9f38be70.chunk.css"
+  },
+  {
+    "revision": "908de87b908771c55d6b",
+    "url": "/dafer154/static/js/2.efdb6ec4.chunk.js"
+  },
+  {
+    "revision": "d455b55662ec59e842e850740eeaf3d2",
+    "url": "/dafer154/static/js/2.efdb6ec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18ef75ec6531ad45c886",
+    "url": "/dafer154/static/js/main.92608f3d.chunk.js"
+  },
+  {
+    "revision": "68c185b050a60f8ed7af",
+    "url": "/dafer154/static/js/runtime-main.4d3a4734.js"
+  },
+  {
+    "revision": "02db4d9ec5e09f8a1e4e9ce91880a07d",
+    "url": "/dafer154/static/media/CV_David_Zuluaga.02db4d9e.pdf"
+  },
+  {
+    "revision": "2ca364f702b7e35d2018cc036cc6bfd0",
+    "url": "/dafer154/static/media/cier-sur.2ca364f7.png"
+  },
+  {
+    "revision": "d83769ee174a9b45e0e6164e2de3c10a",
+    "url": "/dafer154/static/media/covid-19.d83769ee.PNG"
+  },
+  {
+    "revision": "01eab6d338aa3952d8185d423b4cd49c",
+    "url": "/dafer154/static/media/davidZ.01eab6d3.JPG"
+  },
+  {
+    "revision": "3a6436de70d709cef3f628db7d0f9951",
+    "url": "/dafer154/static/media/ortho.3a6436de.png"
+  },
+  {
+    "revision": "0c39cac6fda6c23e8e1b217b2c72d0b1",
+    "url": "/dafer154/static/media/poke-api.0c39cac6.PNG"
+  },
+  {
+    "revision": "f32fb1f2d8048fa602704667ea7842a6",
+    "url": "/dafer154/static/media/portfolio-david.f32fb1f2.png"
+  },
+  {
+    "revision": "f997888d7514967997b7caa3f13ff30e",
+    "url": "/dafer154/static/media/profile.f997888d.jpg"
+  },
+  {
+    "revision": "56bffa12ed34b303900e65d85ae36cbb",
+    "url": "/dafer154/static/media/starmovielist.56bffa12.png"
+  }
+]);
